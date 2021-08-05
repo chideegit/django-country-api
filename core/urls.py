433d61544209a0c3dfae7bc6357f192dfb9837da
chideegit <chidiebereohiri@gmail.com>
2021-08-05 14:09:36 +0100

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import country_list
+
+urlpatterns = [
+	path('', country_list, name='country_list')
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
+from django.http import response
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def country_list(request):
+	response = requests.get("https://restcountries.eu/rest/v2/all").json()
+	context = {'response':response}
+	return render(request, 'country_list.html', context)
+
